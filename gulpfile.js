2c7a2f27e9f6e15c7cfd073d8c50f8faf5e4faca
Joakim Ljungren <joakim@ljungren.se>
2017-01-24 16:12:05 +0100

    :kiss:

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+// Dev tasks
+
+gulp.task('connect', function () {
+  connect.server({
+    name: 'gulp-test',
+    root: ['dev'],
+    port: 9000,
+    livereload: true
+  });
+});
+
+gulp.task('copy', function () {
+  gulp.src([ './app/*.html'])
+    .pipe(gulp.dest('./dev'));
+  gulp.src([ './app/js/*.js' ])
+    .pipe(gulp.dest('./dev/js'));
+});
+ 
+gulp.task('sass', function () {
+  return gulp.src(['./app/style/*.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dev/style'))
+    .pipe(connect.reload());
+});
+gulp.task('html', function () {
+  gulp.src(['./app/*.html'])
+    .pipe(gulp.dest('./dev'))
+    .pipe(connect.reload());
+});
+gulp.task('js', function () {
+  gulp.src(['./app/js/*.js'])
+    .pipe(gulp.dest('./dev/js'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./app/style/*.scss', ['sass']);
+  gulp.watch('./app/js/*.js', ['js']);
+  gulp.watch('./app/*.html', ['html']);
+});
+
+// Build tasks
+
+gulp.task('build', function(){
+  gulp.src('./app/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+
+gulp.task('default', ['sass', 'copy', 'connect', 'watch']);
