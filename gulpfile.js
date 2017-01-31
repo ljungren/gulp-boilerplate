80183c62416cb5437d9509a054d7f8e80c5040a9
Joakim Ljungren <joakim@ljungren.se>
2017-02-01 00:44:23 +0100

    :kissing_smiling_eyes:

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,21 @@ var gulp = require('gulp'),
   open = require('gulp-open'),
   cleanCSS = require('gulp-clean-css');
 
-// Dev tasks
-
-gulp.task('connect', function () {
-  connect.server({
-    name: 'gulp-test',
+var config = {
+  server: {
+    name: 'gulp-boilerplate',
     root: ['public'],
     port: 9000,
     livereload: true
-  });
+  },
+  url: {
+    uri: 'http://localhost:9000'
+  }
+};
+
+/* Dev tasks */
+gulp.task('connect', function () {
+  connect.server(config.server);
 });
  
 gulp.task('sass', function () {
@@ -39,27 +45,27 @@ gulp.task('images', function () {
   gulp.src(['./src/images/*'])
     .pipe(gulp.dest('./public/images'));
 });
+gulp.task('fonts', function () {
+  gulp.src(['./src/style/*.otf', './src/style/*.ttf'])
+    .pipe(gulp.dest('./public/style'));
+});
 gulp.task('open', function(){
-  var options = {
-    uri: 'http://localhost:9000'
-  };
   gulp.src('./public/index.html')
-  .pipe(open(options));
+  .pipe(open(config.url));
 });
 gulp.task('livereload', function() {
   gulp.src(['./src/**/*'])
     .pipe(connect.reload());
 });
-
 gulp.task('watch', function(){
   gulp.watch('./src/style/*.scss', ['sass', 'livereload']);
   gulp.watch('./src/js/*.js', ['jshint', 'js', 'livereload']);
   gulp.watch('./src/*.html', ['html', 'livereload']);
   gulp.watch('./src/images/*', ['images', 'livereload']);
+  gulp.watch(['./src/style/*.otf', './src/style/*.ttf'], ['fonts', 'livereload']);
 });
 
-// Build tasks
-
+/* Build tasks */
 gulp.task('js:build', function(){
   gulp.src(['./src/js/*.js'])
     .pipe(uglify())
@@ -79,8 +85,12 @@ gulp.task('img:build', function () {
   gulp.src(['./src/images/*'])
     .pipe(gulp.dest('./dist/images'));
 });
+gulp.task('assets:build', function () {
+  gulp.src(['./src/style/*.otf', './src/style/*.ttf'])
+    .pipe(gulp.dest('./dist/style'));
+});
   
 
 
-gulp.task('default', ['jshint', 'sass', 'html', 'js', 'images', 'connect', 'watch', 'open']);
-gulp.task('build', ['style:build', 'html:build', 'js:build', 'img:build']);
+gulp.task('default', ['jshint', 'sass', 'fonts', 'html', 'js', 'images', 'connect', 'watch', 'open']);
+gulp.task('build', ['style:build', 'html:build', 'js:build', 'img:build', 'assets:build']);
